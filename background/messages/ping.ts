2a55b5aecce611a80ab14cc5b9beb6e46c713e7b
dangchinh25 <dangchinh2501@gmail.com>
2024-07-18 18:30:34 -0700

    working prototype

--- background/messages/ping.ts
+++ background/messages/ping.ts
@@ -1,4 +1,5 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
+import OpenAI from "openai";
 import { YoutubeTranscript } from "youtube-transcript";
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
@@ -9,6 +10,30 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
       );
 
     console.log(transcriptResponses)
+
+    console.log('key', process.env.PLASMO_PUBLIC_OPENAI_API_KEY)
+
+    const contents = transcriptResponses.map(chunk => chunk.text)
+
+    const openai = new OpenAI({
+        apiKey: process.env.PLASMO_PUBLIC_OPENAI_API_KEY
+    })
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4o',
+        messages: [
+            {
+                role: "system",
+                content: `Act as a helpful assistant that gives detail info about Youtube video given a transcript:
+                Transcripts: ${contents}`
+            }, {
+                role: 'user',
+                content: 'What is this video about?'
+            }
+        ]
+    })
+
+    console.log(response.choices[0].message.content)
 }
  
 export default handler
