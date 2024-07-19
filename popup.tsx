cc8009207b289448ad2c1078b409bbd94a145230
dangchinh25 <dangchinh2501@gmail.com>
2024-07-18 20:28:59 -0700

    styling

--- popup.tsx
+++ popup.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 
+import "./style.css"
+
+import { TailSpin } from "react-loader-spinner"
+
 import { sendToBackground } from "@plasmohq/messaging"
 
 import type {
@@ -10,8 +14,11 @@ import type {
 
 function IndexPopup() {
   const [summary, setSummary] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleGenerateSummary = async () => {
+    setIsLoading(true)
+
     const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
     const currentUrl = tabs[0].url
 
@@ -29,22 +36,30 @@ function IndexPopup() {
         `An error occurred while generating the summary. Errors: ${JSON.stringify((response as GenerateSummaryErrorResponse).errors)}`
       )
     }
+    setIsLoading(false)
   }
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <button onClick={handleGenerateSummary}>Generate summary</button>
-      {summary && <p>{summary}</p>}
+    <div className="w-96 p-4 flex flex-col">
+      <h1 className="text-2xl font-normal text-center">Stop Yapping</h1>
+
+      {!isLoading && (
+        <button
+          className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          onClick={handleGenerateSummary}>
+          Generate summary
+        </button>
+      )}
+      {isLoading && (
+        <div className="flex justify-center mt-2">
+          <TailSpin height={50} width={50} color="#c4c5c7" />
+        </div>
+      )}
+      {summary && !isLoading && (
+        <div className="whitespace-pre-wrap mt-2">
+          <p>{summary}</p>
+        </div>
+      )}
     </div>
   )
 }
